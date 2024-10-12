be18ec67702e8f02f177bef04a1a777d26a282a2
unknown <Mhmdsnalif>
2024-10-12 12:43:03 +0700

    commit pertama

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list'; 
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgFor,
+    NgIf,
+    FormsModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todo-app';
+  task: any;
+  tasks: string[] = [];
+  editingIndex: number | null = null;
+  editTaskValue: string = '';
+
+  addTask() {
+    this.tasks.push(this.task);
+    this.task = '';
+  }
+
+  removeTask(i: number) {
+    this.tasks.splice(i, 1);
+  }
+
+  editTask(i: number) {
+    this.editingIndex = i;
+    this.editTaskValue = this.tasks[i];
+  }
+
+  saveTask() {
+    if (this.editingIndex !== null && this.editTaskValue.trim() !== '') {
+      this.tasks[this.editingIndex] = this.editTaskValue;
+    }
+    this.cancelEdit();
+  }
+
+  cancelEdit() {
+    this.editingIndex = null;
+    this.editTaskValue = '';
+  }
 }
